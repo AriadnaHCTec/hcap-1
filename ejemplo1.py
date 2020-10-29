f99b82e40ac99c59e5f298802ea5dd2fdd46b07f
AriadnaHCTec <arihc@AriNueva.localdomain>
2020-10-29 13:32:33 -0600

    grises

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+
 imagen = cv2.imread("imagen.jpg")
 imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)
 print(imagen.shape)
@@ -16,3 +18,10 @@ imagen[0][1]=0
 imagen[0][2]=0
 
 cv2.imwrite("grayimagen.jpg", imagen)
+
+matriz = np.zeros((256,256),np.float32)
+print(matriz.shape)
+cv2.imwrite("matrizimagen.jpg", matriz)
+imagen = cv2.cvtColor(matriz, cv2.COLOR_GRAY2BGR)
+print(imagen.shape)
+cv2.imwrite("matriz2colorimagen.jpg",imagen)
